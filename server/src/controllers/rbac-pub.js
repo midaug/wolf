050d4d7fd85b7e859a279e8618e3a51b0bb1c043
midaug <days0814@gmail.com>
2022-08-02 11:24:00 +0800

    _accessCheckInternal 校验成功时不再打印日志

--- server/src/controllers/rbac-pub.js
+++ server/src/controllers/rbac-pub.js
@@ -72,14 +72,14 @@ class RbacPub extends BasicService {
       this.ctx.resource = resource;
       const permID = resource.permID;
       if (permID === constant.SystemPerm.ALLOW_ALL) { // allow all user access
-        this.log4js.info('resource {appID: %s, action: %s, resName: %s} permission is [%s], allow all user to access!', appID, action, resName, permID)
+        // this.log4js.info('resource {appID: %s, action: %s, resName: %s} permission is [%s], allow all user to access!', appID, action, resName, permID)
         this.success(data)
       } else if (permID === constant.SystemPerm.DENY_ALL) { // deny all user access
         this.log4js.info('resource {appID: %s, action: %s, resName: %s} permission is [%s], not allow any user to access!', appID, action, resName, permID)
         const reason = util.format(this.ctx.i18n.__('ERR_RBAC_ACCESS_FAILURE_DENY_ALL'), {resName})
         this.fail(403, reason, data)
       } else if (userInfo.permissions[permID]) { // have permission
-        this.log4js.info('user [%s] have permission [%s] to access {appID: %s, action: %s, resName: %s}', userInfo.username, permID, appID, action, resName)
+        // this.log4js.info('user [%s] have permission [%s] to access {appID: %s, action: %s, resName: %s}', userInfo.username, permID, appID, action, resName)
         this.success(data)
       } else { // have no permission
         this.log4js.info('user [%s] have no permission [%s] to access {appID: %s, action: %s, resName: %s}', userInfo.username, permID, appID, action, resName)
